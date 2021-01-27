6d36d6a51f58029460e39a371a20e96f5da43799
=sherwin <=9940511@qq.com>
2021-01-27 15:44:06 +0800

    heihei

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './css/admin.less'
+export default class Admin extends Component {
+
+
+    render() {
+        const { collapsed } = this.state;
+        return (
+            
+        );
+    }
+}
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+import { Form, Input, Button, Checkbox } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+import './css/login.less'
+import qr from './images/QR_code_new3.png'
+export default class Login extends Component {
+    onFinish = (values) => {
+        console.log(values);
+        this.props.history.push('/admin')
+    }
+    render() {
+        return (
+            <section className='login'>
+                <img src={qr} alt="qr" className='qr' />
+                <div className="login_container">
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{
+                            remember: true,
+                        }}
+                        onFinish={this.onFinish}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入用户名!',
+                                },
+                            ]}
+                        >
+                            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="用户名" />
+                        </Form.Item>
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: '请输入密码!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="密码"
+                            />
+                        </Form.Item>
+                        <Form.Item>
+                            <Form.Item name="remember" valuePropName="checked" noStyle>
+                                <Checkbox>记住我</Checkbox>
+                            </Form.Item>
+                            <a className="login-form-forgot" href="">
+                                忘记密码</a>
+                        </Form.Item>
+                        <Form.Item>
+                            <Button shape="round" type="primary" htmlType="submit" className="login-form-button" block >登录</Button>
+                        </Form.Item>
+                    </Form>
+                </div>
+            </section >
+        )
+    }
+}
